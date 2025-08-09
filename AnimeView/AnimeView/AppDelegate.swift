23f4bcda35783fa70787d48b3c9e6f2da480cefd
L3b1n <binleo07@icloud.com>
2025-08-09 14:07:54 +0300

    Fix class naming

--- AnimeView/AnimeView/AppDelegate.swift
+++ AnimeView/AnimeView/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    window?.rootViewController = UpscalingViewController()
+    window?.rootViewController = AnimeViewController()
     window?.makeKeyAndVisible()
     return true
   }
